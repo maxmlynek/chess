aa1fdbeeb2c761b34426b5a76704369f34f6d0d3
Max <maxmlynekfb@gmail.com>
2017-10-09 20:44:39 +0200

    add board filling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 var board = createRectangularBoard(8, 8); 
 fillChessBoard(board);
 
-for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board[i].length; j++) {
-        console.log(board[i][j].code);    
-    }
-}
+// for (var i = 0; i < board.length; i++) {
+//     for (var j = 0; j < board[i].length; j++) {
+//         console.log(board[i][j].code + " ");   
+//         console.log(board[i][j].color + " ");           
+//         if(board[i][j].piece) console.log(board[i][j].piece.name);  
+//     }
+// }
--- js/services.js
+++ js/services.js
@@ -9,7 +9,20 @@ var createRectangularBoard = function (rows, columns) {
 var fillChessBoard = function (board) {
     for (var i = 0, charCode = 65; i < board.length; i++, charCode++) {
         for (var j = 1; j <= board[i].length; j++) {
-            board[i][j-1] = new Square(String.fromCharCode(charCode).toLowerCase() + j);    
+            board[i][j-1] = new Square(String.fromCharCode(charCode).toLowerCase() + j);   
+            if((charCode + j)%2 === 0) {
+                board[i][j-1].color = "dark";
+            } else {
+                board[i][j-1].color = "light";
+            }
+            if(j===2 || j===7) board[i][j-1].piece = new Pawn();
+            if(j===1 || j===8) {
+                if(charCode === 65 || charCode === 72) board[i][j-1].piece = new Rook();
+                if(charCode === 66 || charCode === 71) board[i][j-1].piece = new Knight();
+                if(charCode === 67 || charCode === 70) board[i][j-1].piece = new Bishop();
+                if(charCode === 68) board[i][j-1].piece = new Queen();
+                if(charCode === 69) board[i][j-1].piece = new King();
+            }
         }
     }
     return board;
